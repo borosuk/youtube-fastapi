3a791cc108c83ccf002680da86d6fe95337acbb2
DarXyde <darxyde@gmail.com>
2022-11-20 12:59:59 +0000

    Code clean-up. Streamlined credentials checking. Fixed /revoke to remove the credentials from session as well.

--- server.py
+++ server.py
@@ -39,7 +39,7 @@ app = FastAPI(
 app.add_middleware(HTTPSRedirectMiddleware)
 app.add_middleware(SessionMiddleware, secret_key=config['APP_SECRET_KEY'])
 
-
+# Add the last path to the session
 class LastPath(HTTPConnection):
     @property
     def last_path(self) -> dict:
@@ -49,15 +49,24 @@ class LastPath(HTTPConnection):
         return self.scope["last_path"]
 
 
-# Entry point
-if __name__ == '__main__':
-    uvicorn.run(
-        'server:app', port=443, host='0.0.0.0',
-        #reload=True,
-        #reload_dirs=['.'],
-        ssl_keyfile=config['SSL_KEYFILE'],
-        ssl_certfile=config['SSL_CERTFILE']
-    )
+def get_credentials(request: Request):    
+    return google.oauth2.credentials.Credentials(**request.session['credentials'])
+
+
+def set_credentials(request: Request, credentials: str):
+    request.session['credentials'] = credentials_to_dict(credentials)
+
+
+def get_youtube(credentials: str):
+    return googleapiclient.discovery.build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
+
+
+def clear_session(request: Request):
+    if 'credentials' in request.session:
+        del request.session['credentials']
+
+    if 'last_path' in request.session:
+        del request.session['last_path']
 
 
 def credentials_to_dict(credentials):
@@ -71,6 +80,17 @@ def credentials_to_dict(credentials):
     }
 
 
+# Entry point
+if __name__ == '__main__':
+    uvicorn.run(
+        'server:app', port=443, host='0.0.0.0',
+        reload=True,
+        #reload_dirs=['.'],
+        ssl_keyfile=config['SSL_KEYFILE'],
+        ssl_certfile=config['SSL_CERTFILE']
+    )
+
+
 @app.get("/")
 async def root(request: Request):
     return {"root": config['APP_TITLE']}
@@ -79,9 +99,7 @@ async def root(request: Request):
 @app.get("/authorize")
 async def authorize(request: Request):
     # Create flow instance to manage the OAuth 2.0 Authorization Grant Flow steps.
-    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-        CLIENT_SECRETS_FILE, scopes=SCOPES
-    )
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(CLIENT_SECRETS_FILE, scopes=SCOPES)
 
     # The URI created here must exactly match one of the authorized redirect URIs for the OAuth 2.0 client, which you configured in the API Console. If this
     # value doesn't match an authorized URI, you will get a 'redirect_uri_mismatch' error
@@ -114,8 +132,7 @@ async def oauth2callback(request: Request):
 
     # Store credentials in the session.
     # ACTION ITEM: In a production app, you likely want to save these credentials in a persistent database instead.
-    credentials = flow.credentials
-    request.session['credentials'] = credentials_to_dict(credentials)
+    set_credentials(request, flow.credentials)
 
     return RedirectResponse(app.url_path_for(request.session.get('last_path', 'root')))
 
@@ -125,13 +142,15 @@ async def revoke(request: Request):
     if 'credentials' not in request.session:
         return {"message": "You need to authorize before testing the code to revoke credentials."}
 
-    credentials = google.oauth2.credentials.Credentials(**request.session['credentials'])
+    credentials = get_credentials(request)
 
     revoke = requests.post(
         'https://oauth2.googleapis.com/revoke',
         params={'token': credentials.token},
         headers={'content-type': 'application/x-www-form-urlencoded'}
     )
+
+    clear_session(request)
 
     status_code = getattr(revoke, 'status_code')
     if status_code == 200:
@@ -142,11 +161,7 @@ async def revoke(request: Request):
 
 @app.get('/clear')
 async def clear_credentials(request: Request):
-    if 'credentials' in request.session:
-        del request.session['credentials']
-
-    if 'last_path' in request.session:
-        del request.session['last_path']
+    clear_session(request)
 
     return RedirectResponse(app.url_path_for('root'))
 
@@ -158,63 +173,68 @@ async def get_channel(request: Request):
         return RedirectResponse(app.url_path_for('authorize'))
 
     # Load credentials from the session.
-    credentials = google.oauth2.credentials.Credentials(**request.session['credentials'])
+    credentials = get_credentials(request)
 
-    youtube = googleapiclient.discovery.build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
+    youtube = get_youtube(credentials)
 
-    channel = youtube.channels().list(mine=True, part='snippet,contentDetails').execute()
-
-    # Save credentials back to session in case access token was refreshed.
-    # ACTION ITEM: In a production app, you likely want to save these credentials in a persistent database instead.
-    request.session['credentials'] = credentials_to_dict(credentials)
-
-    return channel
-
-
-@app.get('/playlists', response_model=PlaylistRoot)
-async def get_playlists(request: Request):
-    if 'credentials' not in request.session:
-        request.session['last_path'] = get_playlists.__name__
-        return RedirectResponse(app.url_path_for('authorize'))
-
-    # Load credentials from the session.
-    credentials = google.oauth2.credentials.Credentials(**request.session['credentials'])
-
-    youtube = googleapiclient.discovery.build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
-
-    playlists = youtube.playlists().list(
-        part="snippet,contentDetails",
-        maxResults=25,
+    channel = youtube.channels().list(
+        part='snippet,contentDetails',
         mine=True
     ).execute()
 
     # Save credentials back to session in case access token was refreshed.
     # ACTION ITEM: In a production app, you likely want to save these credentials in a persistent database instead.
-    request.session['credentials'] = credentials_to_dict(credentials)
+    set_credentials(request, credentials)
+
+    return channel
+
+
+@app.get('/playlists', response_model=PlaylistRoot)
+async def get_playlists(request: Request, next_page: str = None, results_per_page: int = 25):
+    if 'credentials' not in request.session:
+        request.session['last_path'] = get_playlists.__name__
+        return RedirectResponse(app.url_path_for('authorize'))
+
+    # Load credentials from the session.
+    credentials = get_credentials(request)
+
+    youtube = get_youtube(credentials)
+
+    playlists = youtube.playlists().list(
+        part="snippet,contentDetails",
+        maxResults=results_per_page,
+        mine=True,
+        pageToken=next_page
+    ).execute()
+
+    # Save credentials back to session in case access token was refreshed.
+    # ACTION ITEM: In a production app, you likely want to save these credentials in a persistent database instead.
+    set_credentials(request, credentials)
 
     return playlists
 
 
 @app.get('/videos', response_model=VideoRoot)
-async def get_videos(request: Request):
+async def get_videos(request: Request, next_page: str = None, results_per_page: int = 25):
     if 'credentials' not in request.session:
         request.session['last_path'] = get_videos.__name__
         return RedirectResponse(app.url_path_for('authorize'))
 
     # Load credentials from the session.
-    credentials = google.oauth2.credentials.Credentials(**request.session['credentials'])
+    credentials = get_credentials(request)
 
-    youtube = googleapiclient.discovery.build(API_SERVICE_NAME, API_VERSION, credentials=credentials)
+    youtube = get_youtube(credentials)
 
     videos = youtube.search().list(
         part="snippet",
         forMine=True,
-        maxResults=25,
-        type="video"
+        maxResults=results_per_page,
+        type="video",
+        pageToken=next_page
     ).execute()
 
     # Save credentials back to session in case access token was refreshed.
     # ACTION ITEM: In a production app, you likely want to save these credentials in a persistent database instead.
-    request.session['credentials'] = credentials_to_dict(credentials)
+    set_credentials(request, credentials)
 
     return videos
